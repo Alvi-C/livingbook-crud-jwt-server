02a825152b8d14ba8d4401429a5b37d30db7f8b6
Alvi Chowdhury <alvic5050@gmail.com>
2023-11-07 11:06:03 +0600

    mongodb conected successfully

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// third party middleware
 app.use(cors());
 app.use(express.json());
 
@@ -14,9 +16,41 @@ app.get('/', (req, res) => {
     res.send('Living Book server is running')
 })
 
+// database
+// Accessing Secrets
+const { MONGO_URI, DB_NAME } = process.env;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+
+        // create database and collections to store data
+        const database = client.db(DB_NAME);
 
 
 
-app.listen(port, () => {
-    console.log(`Living Book server is running on port ${port}`)
-})
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // If MongoDB connection is successful, start the server
+        app.listen(port, () => {
+            console.log(`Living Book server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+run().catch(console.dir);
